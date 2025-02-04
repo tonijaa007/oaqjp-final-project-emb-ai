108e4cce3bbd8ed0089debc183a9315320d77f98
tonijaa007 <tonijaa007@hotmail.com>
2025-02-04 05:10:38 -0500

    Create Emotion Detector

---
+++ emotion_detection.py
@@ -0,0 +1,16 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    
+    response = requests.post(url, headers=headers, json=input_json)
+    response_json = response.json()
+    
+    # Extract the emotion predictions
+    if 'emotionPredictions' in response_json:
+        emotions = response_json['emotionPredictions'][0]['emotion']
+        return emotions
+    else:
+        return 'No emotion detected'
